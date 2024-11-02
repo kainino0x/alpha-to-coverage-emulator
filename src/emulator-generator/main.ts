ed254d58e43f2377661477a51db569eed02dcf54
Kai Ninomiya <kainino@chromium.org>
2024-11-02 15:37:21 -0700

    add block size detection

--- src/emulator-generator/main.ts
+++ src/emulator-generator/main.ts
@@ -4,7 +4,8 @@ import copyMaskToBufferWGSL from './CopyMaskToBuffer.wgsl';
 
 const output = document.getElementById('output')! as HTMLPreElement;
 
-const kSize = 8;
+// Render target size. It's the maximum pattern size we can detect.
+const kSize = 16;
 const kSampleCount = 4;
 const kAlphaIncrements = 10_000;
 
@@ -137,40 +138,79 @@ let lastSeenPatternString = '';
 }
 
 // Try to determine a denominator for the alpha values we saw.
-let halfDenominator = kAlphaIncrements; // fallback denominator
+let halfDenominator = kAlphaIncrements; // use this if we can't find better
 const kAllowedError = 1 / kAlphaIncrements;
 {
-  for (const d of [3, 4, 5, 15, 16, 17, 255, 256, 257, 1023, 1024, 1025]) {
-    let valid = true;
-    for (let i = 1; i < results.length; ++i) {
+  const kCandidateDenominators = [
+    // Powers of 4, plus or minus 1
+    3, 4, 5, 15, 16, 17, 63, 64, 65, 255, 256, 257, 1023, 1024, 1025,
+  ];
+  dLoop: for (const d of kCandidateDenominators) {
+    // Check if this denominator works for all results
+    for (let i = 0; i < results.length; ++i) {
       const { startAlpha } = results[i];
       const numerator = Math.floor(startAlpha * d * 2) / 2;
       if (startAlpha - numerator / d > kAllowedError) {
-        valid = false;
-        break;
+        continue dLoop;
       }
     }
 
-    if (valid) {
-      halfDenominator = d;
-      break;
-    }
+    // If we haven't continue'd, we found a good value!
+    halfDenominator = d;
+    break;
   }
 }
 
-let infoString = `${info.vendor} ${info.architecture} ${info.device} ${info.description}`.trim();
+// Try determine a smaller pattern size than the one we captured.
+let patternSize = kSize; // use this if we can't find better
+sLoop: for (let s = 1; s < kSize; s *= 2) {
+  // Check if this pattern size works for all results
+  for (let i = 0; i < results.length; ++i) {
+    const pattern = results[i].pattern;
+    // (lx,ly) is a "local" coordinate inside the first block
+    for (let ly = 0; ly < s; ++ly) {
+      for (let lx = 0; lx < s; ++lx) {
+        const maskInFirstBlock = pattern[ly * kSize + lx];
+        for (let y = ly + s; y < kSize; y += s) {
+          for (let x = lx + s; x < kSize; x += s) {
+            if (pattern[y * kSize + x] !== maskInFirstBlock) {
+              continue sLoop;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // If we haven't continue'd, we found a good value!
+  patternSize = s;
+  break;
+}
+
+// Generate the shader!
+let infoString =
+  `${info.vendor} ${info.architecture} ${info.device} ${info.description}`.trim();
 let out = `\
 // ${infoString}
 fn emulatedAlphaToCoverage(alpha: f32, xy: vec2u) -> u32 {
-  let i = (xy.y % ${kSize}) * ${kSize} + (xy.x % ${kSize});
+  let i = (xy.y % ${patternSize}) * ${patternSize} + (xy.x % ${patternSize});
 `;
 for (let i = 0; i < results.length - 1; ++i) {
-  const pattern = results[i].pattern;
   const endAlpha = results[i + 1].startAlpha;
+  const capturedPattern = results[i].pattern;
 
-  const alphaFraction = Math.round(endAlpha * halfDenominator * 2) / 2;
+  // Extract the patternSize-sized pattern from the captured result.
+  const pattern = [];
+  for (let y = 0; y < patternSize; ++y) {
+    for (let x = 0; x < patternSize; ++x) {
+      pattern.push(capturedPattern[y * kSize + x]);
+    }
+  }
+
+  const alphaNumerator = Math.round(endAlpha * halfDenominator * 2) / 2;
+  const alphaFraction = `${alphaNumerator} / ${halfDenominator}.0`;
   const array = Array.from(pattern, (v) => '0x' + v.toString(16)).join(', ');
-  out += `  if (alpha < ${alphaFraction} / ${halfDenominator}.0) { return array(${array}u)[i]; }\n`;
+  out += `  if (alpha < ${alphaFraction}) { return array(${array}u)[i]; }\n`;
 }
 out += `\
   return 0xf;
